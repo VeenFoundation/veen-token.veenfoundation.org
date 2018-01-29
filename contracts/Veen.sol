54accdde3716717b9a12de620be084bfbee373ea
이도현 <cailhuiris@gmail.com>
2018-01-29 15:29:23 +0900

    initial commit

---
+++ contracts/Veen.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.17;
+
+/*
+
+    Copyright 2018, All rights reserved.
+     _      _
+    \ \    / /  ___   ___  _ __
+     \ \  / /  / _ \ / _ \| '_ \
+      \ \/ /  |  __/|  __/| | | |
+       \__/    \___| \___||_| |_|
+
+    @title Veen Token Contract.
+    @author Dohyeon Lee
+    @description Veen token is a ERC20-compliant token.
+
+*/
+
+contract Veen {
+
+}
