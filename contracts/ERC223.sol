d9416e931593ea2d3791a0206cd5fdb89390094a
Dongyounglee <freesam02@naver.com>
2018-08-20 09:57:28 +0900

    Lock-up code + Merge source code

--- contracts/ERC223.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.18;
-interface ERC223 {
-
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address who) public constant returns (uint);
-    function transfer(address to, uint value) public returns (bool);
-
-}
--- contracts/Veen.sol
+++ contracts/Veen.sol
@@ -1,10 +1,256 @@
 pragma solidity ^0.4.18;
 
-import "./ERC20Token.sol";
-import "./SafeMath.sol";
-import "./Pausable.sol";
-import "./ERC223.sol";
-import "./Receiver_Interface.sol";
+// File: contracts/SafeMath.sol
+
+/*
+
+    Copyright 2018, All rights reserved.
+     _      _
+    \ \    / / ___   ___  _ __
+     \ \  / / / _ \ / _ \| '_ \
+      \ \/ / |  __/|  __/| | | |
+       \__/   \___| \___||_| |_|
+
+    @title SafeMath
+    @author OpenZeppelin
+    @dev Math operations with safety checks that throw on error
+
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+// File: contracts/Ownable.sol
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+
+contract Ownable {
+  address public owner;
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+  using SafeMath for uint256;
+  uint256 public startdate;
+
+  function Ownable() public {
+
+    owner = msg.sender;
+    startdate = now;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+
+}
+
+// File: contracts/Pausable.sol
+
+/**
+ * @title Pausable
+ * @dev Base contract which allows children to implement an emergency stop mechanism.
+ */
+contract Pausable is Ownable {
+  event Pause();
+  event Unpause();
+  mapping(address => uint256) private _lock_list_period;
+  mapping(address => bool) private _lock_list;
+  bool public paused = false;
+  mapping(address => uint256) internal _balances;
+  uint256 internal _tokenSupply;
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is not paused.
+   */
+  modifier whenNotPaused() {
+    require(!paused);
+    _;
+  }
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is paused.
+   */
+  modifier whenPaused() {
+    require(paused);
+    _;
+  }
+  /**
+   *
+   */
+
+
+
+  modifier isLockAddress() {
+    check_lock_period(msg.sender);
+    if(_lock_list[msg.sender]){
+        revert();
+    }
+
+    _;
+
+  }
+
+  function check_lock_period(address check_address) {
+      if(now > _lock_list_period[check_address] && _lock_list[check_address]){
+        _lock_list[check_address] = false;
+        _tokenSupply = _tokenSupply.add(_balances[check_address]);
+      }
+
+  }
+
+  function check_period(address check_address) constant public returns(uint256){
+      return _lock_list_period[check_address];
+
+  }
+
+  function check_lock(address check_address) constant public returns(bool){
+
+      return _lock_list[check_address];
+
+  }
+  /**
+   *
+   */
+  function set_lock_list(address lock_address, uint period) onlyOwner external {
+      _lock_list_period[lock_address] = startdate + (period * 1 days);
+      _lock_list[lock_address]  = true;
+      _tokenSupply = _tokenSupply.sub(_balances[lock_address]);
+  }
+
+  /**
+   * @dev called by the owner to pause, triggers stopped state
+   */
+  function pause() onlyOwner whenNotPaused public {
+    paused = true;
+    Pause();
+  }
+  /**
+   * @dev called by the owner to unpause, returns to normal state
+   */
+  function unpause() onlyOwner whenPaused public {
+    paused = false;
+    Unpause();
+  }
+}
+
+// File: contracts/ERC20Token.sol
+
+/*
+
+    Copyright 2018, All rights reserved.
+     _      _
+    \ \    / / ___   ___  _ __
+     \ \  / / / _ \ / _ \| '_ \
+      \ \/ / |  __/|  __/| | | |
+       \__/   \___| \___||_| |_|
+
+    @title Veen Token Contract.
+    @description ERC-20 Interface
+
+*/
+
+interface ERC20Token {
+
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+// File: contracts/ERC223.sol
+
+interface ERC223 {
+
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address who) public constant returns (uint);
+    function transfer(address to, uint value) public returns (bool);
+
+}
+
+// File: contracts/Receiver_Interface.sol
+
+/*
+ * Contract that is working with ERC223 tokens
+ */
+
+ contract ContractReceiver {
+
+    struct TKN {
+        address sender;
+        uint value;
+        bytes data;
+        bytes4 sig;
+    }
+
+
+    function tokenFallback(address _from, uint _value, bytes _data) public pure {
+      TKN memory tkn;
+      tkn.sender = _from;
+      tkn.value = _value;
+      tkn.data = _data;
+      uint32 u = uint32(_data[3]) + (uint32(_data[2]) << 8) + (uint32(_data[1]) << 16) + (uint32(_data[0]) << 24);
+      tkn.sig = bytes4(u);
+
+      /* tkn variable is analogue of msg variable of Ether transaction
+      *  tkn.sender is person who initiated this token transaction   (analogue of msg.sender)
+      *  tkn.value the number of tokens that were sent   (analogue of msg.value)
+      *  tkn.data is data of token transaction   (analogue of msg.data)
+      *  tkn.sig is 4 bytes signature of function
+      *  if data of token transaction is a function execution
+      */
+    }
+}
+
+// File: contracts/Veen.sol
 
 /*
     Copyright 2018, All rights reserved.
@@ -15,7 +261,6 @@ import "./Receiver_Interface.sol";
        \__/   \___| \___||_| |_|
 
     @title Veen Token Contract.
-    @author Dohyeon Lee
     @description Veen token is a ERC20-compliant token.
 
 */
@@ -27,9 +272,9 @@ contract Veen is ERC20Token, Pausable, ERC223{
     string public constant name = "Veen";
     string public constant symbol = "VEEN";
     uint8 public constant decimals = 18;
-    uint private _tokenSupply;
+
     uint private _totalSupply;
-    mapping(address => uint256) private _balances;
+
     mapping(address => mapping(address => uint256)) private _allowed;
     event MintedLog(address to, uint256 amount);
     event Transfer(address indexed from, address indexed to, uint value);
@@ -51,7 +296,7 @@ contract Veen is ERC20Token, Pausable, ERC223{
         if(_totalSupply + 1 > (_tokenSupply+amount)){
             _tokenSupply = _tokenSupply.add(amount);
             _balances[to]= _balances[to].add(amount);
-            MintedLog(to, amount);
+            emit MintedLog(to, amount);
             return true;
         }
 
@@ -69,7 +314,7 @@ contract Veen is ERC20Token, Pausable, ERC223{
         return _balances[tokenOwner];
     }
 
-    function transfer(address to, uint tokens) whenNotPaused public returns(bool success){
+    function transfer(address to, uint tokens) whenNotPaused isLockAddress public returns(bool success){
     bytes memory empty;
     	if(isContract(to)) {
         	return transferToContract(to, tokens, empty);
@@ -84,7 +329,7 @@ contract Veen is ERC20Token, Pausable, ERC223{
 
         if (tokens > 0 && balanceOf(msg.sender) >= tokens) {
             _allowed[msg.sender][spender] = tokens;
-            Approval(msg.sender, spender, tokens);
+            emit Approval(msg.sender, spender, tokens);
             return true;
         }
 
@@ -100,7 +345,7 @@ contract Veen is ERC20Token, Pausable, ERC223{
             _balances[from] = _balances[from].sub(tokens);
             _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(tokens);
             _balances[to] = _balances[to].add(tokens);
-            Transfer(msg.sender, to, tokens);
+            emit Transfer(msg.sender, to, tokens);
             return true;
         }
         return false;
@@ -122,7 +367,7 @@ contract Veen is ERC20Token, Pausable, ERC223{
     emit Transfer(msg.sender, _to, _value);
     return true;
   }
-  
+
   //function that is called when transaction target is a contract
   function transferToContract(address _to, uint _value, bytes _data) private returns (bool success) {
     if (balanceOf(msg.sender) < _value) revert();
